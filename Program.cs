109a97d4a1b151bae07dde3fa8644a4423e36a91
Vladimir <rybalkinv@inbox.ru>
2023-02-18 15:13:25 +0300

    дз6, готовый файл

---
+++ Program.cs
@@ -0,0 +1,117 @@
+﻿int programm, num;
+Boolean begin = true;
+ 
+while (begin)
+{
+Console.WriteLine("------");
+Console.WriteLine("Введите число для соответствующей задачи или иное для выхода:");
+Console.WriteLine("1. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.");
+Console.WriteLine("2. Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.");
+
+programm = Convert.ToInt32(Console.ReadLine());
+ 
+switch (programm)
+{
+case 1:
+// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Write("Введите числа через запятую: ");
+int[] numbers = StringToNum(Console.ReadLine());
+PrintArray(numbers);
+int sum = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+    {
+        sum++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"количество значений больше 0 = {sum}");
+
+
+int[] StringToNum(string input)
+{
+    int count = 1;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == ',')
+        {
+            count++;
+        }
+    }
+
+    int[] numbers = new int [count];
+    int index = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        string temp = "";
+
+        while (input [i] != ',')
+        {
+        if(i != input.Length - 1)
+        {
+            temp += input [i].ToString();
+            i++;
+        }
+        else
+        {
+            temp += input [i].ToString();
+            break;
+        }
+        }
+        numbers[index] = Convert.ToInt32(temp);
+        index++;
+    }
+    return numbers;
+}
+
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write("]");
+}
+
+break;
+
+case 2:
+//Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+double pointX;
+double pointY;
+
+Console.Write("Даны прямые заданные формулами y = k1 * x + b1, y = k2 * x + b2");
+Console.Write("Введите по очереди b1, k1, b2 и k2 одной строкой через пробел: ");
+int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+int a = arr [1];
+int b = arr [3];
+int c = arr [0];
+int d = arr [2];
+
+if (a == b)
+{
+    Console.WriteLine("Прямые с такими параметрами паралельны");
+}
+else
+{
+    pointX = (double) (d-c)/(a-b);
+    pointY = (double) a*(d-c)/(a-b) + c;
+    Console.WriteLine("Координаты точки пересечения двух прямых:");
+    Console.WriteLine($"Х: {pointX:0.00}");
+    Console.WriteLine($"Y: {pointY:0.00}");
+}
+
+break;
+
+
+
+
+default:
+begin = false;
+break;
+}
+}
